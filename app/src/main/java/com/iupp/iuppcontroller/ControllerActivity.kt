c8b8e171007726a21f3377cc6e79263d05eb7270
0442A403 <petrovdamir2235@gmail.com>
2018-03-28 16:03:50 +0500

    initial commit

---
+++ app/src/main/java/com/iupp/iuppcontroller/ControllerActivity.kt
@@ -0,0 +1,23 @@
+package com.iupp.iuppcontroller
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class ControllerActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_controller)
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
