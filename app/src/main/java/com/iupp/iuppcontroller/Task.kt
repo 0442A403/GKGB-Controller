9cba1e648db08b96c9192a17b84a0b537a383179
0442A403 <petrovdamir2235@gmail.com>
2018-06-13 02:40:42 +0500

    Delete unnecessary files

--- app/src/main/java/com/iupp/iuppcontroller/Task.kt
+++
@@ -1,9 +0,0 @@
-package com.iupp.iuppcontroller
-
-enum class Task(val command: String) {
-    SitDown("COMMAND_SIT_DOWN"),
-    StandUp("COMMAND_STAND_UP"),
-    MakeAParty("COMMAND_MAKE_PARTY"),
-    Stop("COMMAND_STOP"),
-    StepForward("COMMAND_STEP_FORWARD")
-}
